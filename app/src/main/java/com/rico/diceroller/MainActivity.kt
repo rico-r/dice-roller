4d8222b28dd36ed0877a501c96957d4f4352a19e
Rico Ronaldo <91813137+rico-r@users.noreply.github.com>
2023-04-05 06:57:34 +0700

    Dice Roller interaktif

--- app/src/main/java/com/rico/diceroller/MainActivity.kt
+++ app/src/main/java/com/rico/diceroller/MainActivity.kt
@@ -2,10 +2,40 @@ package com.rico.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
+/**
+ * Activity ini memungkinkan pegguna untuk mengocok dadu dan
+ * menampilkan hasilnya ke layar.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener{ rollDice() }
+    }
+
+    /**
+     * Mengocok dadu dan memperbarui tampilan layar dengan hasilnya
+     */
+    private fun rollDice() {
+        // Membuat objek Dice dengan 6 sisi dan mengocoknya
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Memperbarui layar dengan hasil kocokan dadu
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
